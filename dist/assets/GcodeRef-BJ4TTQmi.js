49dba1c4143e68ff40745bc22188f7b0a95a4e9c
Paralaps <kolyhan@gmail.com>
2026-09-15 20:04:16 +0000

    Сборка 1.6.40 (дополнение 3): ASKUP/Torkoal — корпусные фрезы, свёрла и зажимная оснастка

--- dist/assets/GcodeRef-BJ4TTQmi.js
+++ dist/assets/GcodeRef-BJ4TTQmi.js
@@ -1,4 +1,4 @@
-import{j as t}from"./react-DyHMKhAQ.js";import{T as d}from"./TreeRef-D2-UPXKC.js";import"./index-DNssFDe_.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const r=`
+import{j as t}from"./react-DyHMKhAQ.js";import{T as d}from"./TreeRef-BSwr4-4W.js";import"./index-DEM3_SiO.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const r=`
 <div class="card">
 <h2>Справочник G-код</h2>
 <p class="sub">Основные G-коды станков с ЧПУ, полные списки кодов Fanuc и Sinumerik с пояснениями и примерами.</p>
