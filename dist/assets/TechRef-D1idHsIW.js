ebb21beb3445f7effa212cb8a299bb5e16fc240c
Paralaps <kolyhan@gmail.com>
2026-09-16 12:33:28 +0000

    Сборка 1.6.51 (доп.): сборка dist после правки чисел в справке (908 сплавов, 297 стружколомов)

--- dist/assets/TechRef-D1idHsIW.js
+++ dist/assets/TechRef-D1idHsIW.js
@@ -1,4 +1,4 @@
-import{r as b,j as a}from"./react-DyHMKhAQ.js";import{T as i,t as e}from"./index-Cxgt1jCJ.js";import{T as c}from"./TreeRef-BERbcVy5.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const p=`
+import{r as b,j as a}from"./react-DyHMKhAQ.js";import{T as i,t as e}from"./index-DYgRcRi6.js";import{T as c}from"./TreeRef-rjONDdUd.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const p=`
 <div class="card">
 <h2>Справочник технолога</h2>
 <p class="sub">Справочник по теории и практике обработки резанием: обозначения, режимы, геометрия инструмента, обрабатываемость материалов, износ и формулы.</p>
