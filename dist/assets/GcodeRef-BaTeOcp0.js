6d1a462c923a59d8dd40eeaba778d5b49894ad37
Paralaps <kolyhan@gmail.com>
2026-09-16 12:00:13 +0000

    Сборка 1.6.50: державки Derek пересобраны из «1. Derek main -2024.pdf» (118 старых серий → 108 новых: наружное точение, расточные, канавочные, резьбовые; применяемая пластина из каталога); индекс державок 297 серий / 2692, 27 кросс-брендовых форм; водяной знак каталога отфильтрован

--- dist/assets/GcodeRef-BaTeOcp0.js
+++ dist/assets/GcodeRef-BaTeOcp0.js
@@ -1,4 +1,4 @@
-import{j as t}from"./react-DyHMKhAQ.js";import{T as d}from"./TreeRef-DONaY5-f.js";import"./index-DHjCRIfv.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const r=`
+import{j as t}from"./react-DyHMKhAQ.js";import{T as d}from"./TreeRef-C4znvWfA.js";import"./index-BSJIeAJt.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const r=`
 <div class="card">
 <h2>Справочник G-код</h2>
 <p class="sub">Основные G-коды станков с ЧПУ, полные списки кодов Fanuc и Sinumerik с пояснениями и примерами.</p>
