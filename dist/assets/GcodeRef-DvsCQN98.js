bdb40466c9e741adc32fec611e8cf87bcc9a3d65
Paralaps <kolyhan@gmail.com>
2026-09-17 12:21:49 +0000

    Сборка 1.7 (1.7.2): фирменный логотип «Колыханка инжиниринг» (шапка, печатный отчёт), анимированная заставка загрузки вместо текстовой заглушки, иконки сайта svg/png/ico/apple-touch-icon; в dist добавлены логотипы и заставка

--- dist/assets/GcodeRef-DvsCQN98.js
+++ dist/assets/GcodeRef-DvsCQN98.js
@@ -1,4 +1,4 @@
-import{j as t}from"./react-DyHMKhAQ.js";import{T as d}from"./TreeRef-WsSbibu9.js";import"./index-Du_S9Qlf.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const r=`
+import{j as t}from"./react-DyHMKhAQ.js";import{T as d}from"./TreeRef-BoyL-vq6.js";import"./index-BKabtIiB.js";import"./radix-B9GY7zd5.js";import"./vendor-Bcd5AAWi.js";import"./data-DyK8EU5O.js";const r=`
 <div class="card">
 <h2>Справочник G-код</h2>
 <p class="sub">Основные G-коды станков с ЧПУ, полные списки кодов Fanuc и Sinumerik с пояснениями и примерами.</p>
